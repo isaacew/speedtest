4883f25f8164c223c15afafcd0ab47e1026ddca5
isaacew <isaacweintraub@gmail.com>
2021-01-01 09:52:47 -0500

    first csv generation

--- speedtest.py
+++ speedtest.py
@@ -3,8 +3,8 @@ import re #Library to allow for doing regular expressions
 import subprocess #We need to be able to call a 2nd python script
 import time #record date and time
 
-# In this line of code, we utilize the subprocess library 
-# to launch a call to the speedtest-cli python script and 
+# In this line of code, we utilize the subprocess library
+# to launch a call to the speedtest-cli python script and
 # tell it to pipe everything from the speedtest to stdout
 response = subprocess.Popen('/usr/local/bin/speedtest-cli --simple', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 
@@ -17,8 +17,8 @@ download = download[0].replace(',', '.')
 upload = upload[0].replace(',', '.')
 
 try:
-    f = open('/home/pi/speedtest/speedtest.csv', 'a+')
-    if os.stat('/home/pi/speedtest/speedtest.csv').st_size == 0:
+    f = open('/Users/weintraub0/Documents/Projects/speedtest/speedtest.csv', 'a+')
+    if os.stat('/Users/weintraub0/Documents/Projects/speedtest/speedtest.csv').st_size == 0:
             f.write('Date,Time,Ping (ms),Download (Mbit/s),Upload (Mbit/s)\r\n')
 except:
     pass
